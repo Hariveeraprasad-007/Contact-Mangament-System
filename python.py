7be807be54a46b149a9aa5be142e1a37a43a6845
Hariveeraprasad-007 <arikatlahariveeraprasad007@gmail.com>
2025-04-22 07:33:01 +0530

    Added new function 'Search by number'

--- python.py
+++ python.py
@@ -64,13 +64,32 @@ def search_contact():
                 found=True
         if not found:
             print("Contact not found")
+def search_by_number():
+    ph=input("please enter the number")
+    found=False
+    if not ph.isdigit() or len(ph)!=10:
+        print("enter valid phone number")
+        return
+    with open(contact,'r') as file:
+        for line in file:
+            parts=line.strip().split(',')
+            if len(parts)!=3:
+                continue
+            fname,lname,phno=parts
+            if phno==ph:
+                print(f"{fname} {lname}")
+                print(f"{phno}")
+                found=True
+    if not found:
+        print("The contact is not found")
 
 def main():
     while True:
         print("1-Add Contacts")
         print("2-Search Conatcts")
         print("3-Display conatcts")
-        print("4-Exit")
+        print("4-Search By Number")
+        print("5-Exit")
         try:
             num=int(input("Please give the number based on above choices"))
         except ValueError:
@@ -83,6 +102,8 @@ def main():
         elif num==3:
             display_contact_info()
         elif num==4:
+            search_by_number()
+        elif num==5:
             print("Exiting Contact Manager")
             break
         else:
